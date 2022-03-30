4c21beca6ea47c4d7bc01e66af32d4ac0839a60e
Stellarnight <41061137+Stellarnight@users.noreply.github.com>
2022-03-30 22:39:33 +0800

    SCP-Base Helper Fxn + Base

--- project1-sps/script.js
+++ project1-sps/script.js
@@ -1,4 +1,33 @@
+//helper function to generate random SCP for the "AI"
+var computerSCP = function () {
+  var randomFloatSCP = Math.random() * 3;
+  var randomIntegerSCP = Math.floor(randomFloatSCP);
+  if (randomIntegerSCP == 0) {
+    var comSCP = "scissor";
+  } else if (randomIntegerSCP == 1) {
+    var comSCP = "paper";
+  } else if (randomIntegerSCP == 2) {
+    var comSCP = "stone";
+  }
+  return comSCP;
+};
+//Actual SCP Game Function
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (!input == "scissor" || "paper" || "stone") {
+    var outputMessage =
+      "Invalid input. Please key in lower case; scissor, paper or stone";
+  }
+  var comSCPOutput = computerSCP();
+  var playerSCP = input;
+  var outputMessage = "You lose!";
+  if (comSCPOutput == playerSCP) {
+    outputMessage = "Draw!";
+  } else if (comSCPOutput == "paper" && playerSCP == "scissor") {
+    outputMessage = "You win!";
+  } else if (comSCPOutput == "stone" && playerSCP == "paper") {
+    outputMessage = "You win!";
+  } else if (comSCPOutput == "scissor" && playerSCP == "stone") {
+    outputMessage = "You win!";
+  }
+  return outputMessage;
 };
