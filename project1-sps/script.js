8c584395b31debca786978e7299a43657ad3d364
Stellarnight <41061137+Stellarnight@users.noreply.github.com>
2022-03-30 22:46:46 +0800

    SCP Part 1 Completed with more comofrtable mode not yet

--- project1-sps/script.js
+++ project1-sps/script.js
@@ -11,7 +11,40 @@ var computerSCP = function () {
   }
   return comSCP;
 };
-//Actual SCP Game Function
+//Reverse Mode SCP
+var reverseSCP = function () {
+  var comSCPOutput = computerSCP();
+  var playerSCP = input;
+  var outputMessage = "You win!";
+  if (comSCPOutput == playerSCP) {
+    outputMessage = "Draw!";
+  } else if (comSCPOutput == "paper" && playerSCP == "scissor") {
+    outputMessage = "You lose!";
+  } else if (comSCPOutput == "stone" && playerSCP == "paper") {
+    outputMessage = "You lose!";
+  } else if (comSCPOutput == "scissor" && playerSCP == "stone") {
+    outputMessage = "You lose!";
+  }
+  return outputMessage;
+};
+//Normal Mode SCP
+var normalSCP = function () {
+  var comSCPOutput = computerSCP();
+  var playerSCP = input;
+  var outputMessage = "You lose!";
+  if (comSCPOutput == playerSCP) {
+    outputMessage = "Draw!";
+  } else if (comSCPOutput == "paper" && playerSCP == "scissor") {
+    outputMessage = "You win!";
+  } else if (comSCPOutput == "stone" && playerSCP == "paper") {
+    outputMessage = "You win!";
+  } else if (comSCPOutput == "scissor" && playerSCP == "stone") {
+    outputMessage = "You win!";
+  }
+  return outputMessage;
+};
+
+//Combined SCP Game Function
 var main = function (input) {
   if (!input == "scissor" || "paper" || "stone") {
     var outputMessage =
