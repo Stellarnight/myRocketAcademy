f6a818ee6d0f2be23e810d75aaf66f17fa02b293
Stellarnight <41061137+Stellarnight@users.noreply.github.com>
2022-04-03 21:49:33 +0800

    A

--- day04-loops/pre-class/script.js
+++ day04-loops/pre-class/script.js
@@ -1,23 +1,31 @@
 var followAlongMain = function (input) {
   // Attempt the Follow Along exercise from the Loops module below with followAlongMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var counter = 0;
+  while (counter < 6) {
+    myOutputValue = myOutputValue + "Hello ";
+    counter += 1;
+  }
+  return myOutputValue;
+
+  var myOutputValue = "";
   return myOutputValue;
 };
 
 var simpleLoopMain = function (input) {
   // Attempt the Simple Loop with Variations exercise from the Loops module below with simpleLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var loopWithinLoopMain = function (input) {
   // Attempt the Loop Within Loop exercise from the Loops module below with loopWithinLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var infiniteLoopMain = function (input) {
   // Attempt the Infinite Loop exercise from the Loops module below with infiniteLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
