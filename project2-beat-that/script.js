4e8f56359a81786903a70ab3e6057b06ecb4b9e4
Stellarnight <41061137+Stellarnight@users.noreply.github.com>
2022-04-07 21:21:38 +0800

    Proj2

--- project2-beat-that/script.js
+++ project2-beat-that/script.js
@@ -1,4 +1,34 @@
+var mode = "Player1Roll"; //default start mode
+
+//dice roller function
+var rollDice = function () {
+  return Math.ceil(Math.random() * 6);
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (mode === "Player1Roll") {
+    var p1Dice1 = rollDice();
+    var p1Dice2 = rollDice();
+    mode = "Player1Choose";
+    return (
+      `Welcome Player 1` +
+      `<br>` +
+      `You rolled ${p1Dice1} and ${p1Dice2}.` +
+      `<br>` +
+      `Choose the order of the dice`
+    );
+  } else if (mode === `Player1Choose`) {
+    if (input === "1") {
+      var p1CombinedDice = p1Dice1 + p1Dice2;
+      mode = `Player2Roll`;
+      return (
+        `Player 1, you chose dice 1 first.` +
+        `<br>` +
+        `Your number is ${p1CombinedDice}. <br> ` +
+        `It is now Player 2's turn.`
+      );
+    }
+  } else if (mode === `Player2Roll`) {
+    return `happiness`;
+  }
 };
