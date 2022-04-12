b91ec1d1f41ca73a5c627063ad2c22f3cbb4746e
Stellarnight <41061137+Stellarnight@users.noreply.github.com>
2022-04-12 22:15:21 +0800

    AAA Work in progress

--- project2-beat-that/script.js
+++ project2-beat-that/script.js
@@ -1,109 +1,49 @@
-var mode = "Player1Roll"; //default start mode
-var p1Dice1 = "A"; //if for some reason, the end results output these letters, it tells me that the code isnt doing what i want
-var p1Dice2 = "B";
-var p2Dice1 = "C";
-var p2Dice2 = "D";
-var p1CombinedScoreArray = [];
-var p1CombinedScore = "E";
-var p2CombinedScoreArray = [];
-var p2CombinedScore = "F";
+//Global Variables
 
-//Scoreboard
-var p1Win = 0;
-var p2Win = 0;
-var totalGames = 0;
+//There are two players and player 1 always start first
+var player = 1; //0 makes it easier for me to put this in an array later
+var mode = `Roll Dices`; //There are two modes: Dice Rolling and Choosing the Dice
+var storeDiceResultsArray = []; //I need a temporary array to store the results of the individual dice roll
+var playerDiceNumber = []; //I need an array to keep the combined number after the players chooses. This will also enable me to do comparison later
 
 //dice roller function
 var rollDice = function () {
   return Math.ceil(Math.random() * 6);
 };
 
+//function to enable player to roll X number of dice and output for player to choose
+var rollXDices = function (input) {
+  var msg = "testing";
+  if (mode === `Roll Dices`) {
+    for (let i = 0; i < input; i++) {
+      storeDiceResultsArray.push(Number(rollDice()));
+      //console.log(storeDiceResultsArray[i]);
+    }
+    //console.log(storeDiceResultsArray);
+    mode = `Choice`; //change mode to choice
+    //console.log(msg);
+    msg = `Welcome Player ${player}! <br> 
+    You rolled ${storeDiceResultsArray.length} dices. Your dice rolls were ${storeDiceResultsArray}. <br>
+    Please choose the order of the dice by keying in the position of the dice i.e if you want dice 1 to be first, key 1 followed by a comma for the next dice `;
+    //console.log(msg);
+    return msg;
+  } else if (mode === `Choice`) {
+    var tempArrayforSplit = input.split(",");
+    for (let j = 0; j < tempArrayforSplit.length; j++) {
+      playerDiceNumber[player].concat(
+        storeDiceResultsArray[tempArrayforSplit[j]]
+      );
+    }
+    mode = "Roll Dices";
+    msg = `Player ${player}. You have chosen the order of dice to be ${tempArrayforSplit}. <br>
+       Your number is ${playerDiceNumber[player]} <br>
+       It is now ${player + 1}'s turn. Press submit to roll dices.`;
+    player += 1;
+    return msg;
+  }
+};
+
 //Main Function
 var main = function (input) {
-  if (mode === "Player1Roll") {
-    p1Dice1 = rollDice();
-    p1Dice2 = rollDice();
-    p1CombinedScoreArray.push(p1Dice1, p1Dice2);
-    mode = "Player1Choose";
-    return (
-      `Welcome Player 1` +
-      `<br>` +
-      `You rolled ${p1Dice1} and ${p1Dice2}.` +
-      `<br>` +
-      `Choose the order of the dice by entering either 1 or 2`
-    );
-  } else if (mode === `Player1Choose`) {
-    if (input == "1") {
-      p1CombinedScore =
-        String(p1CombinedScoreArray[0]) + String(p1CombinedScoreArray[1]);
-      mode = `Player2Roll`;
-      return (
-        `Player 1, you chose dice 1 first.` +
-        `<br>` +
-        `Your number is ${p1CombinedScore}. <br> ` +
-        `It is now Player 2's turn.`
-      );
-    } else if (input == "2") {
-      p1CombinedScore =
-        String(p1CombinedScoreArray[1]) + String(p1CombinedScoreArray[0]);
-      mode = `Player2Roll`;
-      return (
-        `Player 1, you chose dice 2 first.` +
-        `<br>` +
-        `Your number is ${p1CombinedScore}. <br> ` +
-        `It is now Player 2's turn.`
-      );
-    }
-  } else if (mode === `Player2Roll`) {
-    p2Dice1 = rollDice();
-    p2Dice2 = rollDice();
-    p2CombinedScoreArray.push(p2Dice1, p2Dice2);
-    mode = "Player2Choose";
-    return (
-      `Welcome Player 2` +
-      `<br>` +
-      `You rolled ${p2Dice1} and ${p2Dice2}.` +
-      `<br>` +
-      `Choose the order of the dice by entering either 1 or 2`
-    );
-  } else if (mode === `Player2Choose`) {
-    if (input == "1") {
-      p2CombinedScore =
-        String(p2CombinedScoreArray[0]) + String(p2CombinedScoreArray[1]);
-      mode = `WinOrLose`;
-      return (
-        `Player 2, you chose dice 2 first.` +
-        `<br>` +
-        `Your number is ${p2CombinedScore}. <br> ` +
-        `Press submit again to see who won`
-      );
-    } else if (input == "2") {
-      p2CombinedScore =
-        String(p2CombinedScoreArray[1]) + String(p2CombinedScoreArray[0]);
-      mode = `WinOrLose`;
-      return (
-        `Player 2, you chose dice 2 first.` +
-        `<br>` +
-        `Your number is ${p2CombinedScore}. <br> ` +
-        `Press submit again to see who won.`
-      );
-    }
-  } else if (mode === "WinOrLose") {
-    var outputMessage = "Draw!";
-    if (p1CombinedScore > p2CombinedScore) {
-      outputMessage = "Player 1 wins!";
-      p1Win += 1;
-      totalGames += 1;
-      return outputMessage;
-    } else if (p1CombinedScore < p2CombinedScore) {
-      p2Win += 1;
-      totalGames += 1;
-      outputMessage = `Player 2 wins! <br> Total Games Played: ${totalGames}.  
-      <br> Player 1 Win: ${p1Win} <br> 
-      Player 2 Win: ${p2Win}`;
-      return outputMessage;
-    }
-    totalGames += 1;
-    return outputMessage;
-  }
+  return rollXDices(input);
 };
