895eedf0a737225f7963030e45d59ff4bdeda50c
Stellarnight <41061137+Stellarnight@users.noreply.github.com>
2022-04-17 23:05:34 +0800

    cardDeck

--- project3-blackjack/script.js
+++ project3-blackjack/script.js
@@ -1,4 +1,35 @@
+//Function to create a deck of 52 cards
+var makeDeck = function () {
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  var cardDeck = [];
+  for (var i = 0; i < suits.length; i++) {
+    for (var j = 1; j < 14; j++) {
+      var card = {
+        name: ``,
+        suit: ``,
+        rank: ``
+      };
+      card.suit = suits[i];
+      console.log(card.suit);
+      card.rank = j;
+      console.log(card.rank);
+      card.name = `${j}`;
+      if (card.rank == 11) {
+        card.name = `Jack`;
+      } else if (card.rank == 12) {
+        card.name = `Queen`;
+      } else if (card.rank == 13) {
+        card.name = `King`;
+      }
+      console.log(card.name);
+      console.log(card);
+      cardDeck.push(card);
+    }
+  }
+  console.log(cardDeck);
+  return cardDeck;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  return makeDeck();
 };
